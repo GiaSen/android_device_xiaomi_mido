a91f4ef63bfa1e6162d05bceddf6b4c6aca9c4bb
Garik Badalyan <garikb@codeaurora.org>
2017-03-31 16:04:42 -0700

    mido: HIDL: Enable compilation of android.hidl.manager@1.0-java
    android.hidl.manager@1.0-java.jar is used by HAL clients
    to register for IServiceNotification.onRegistration()
    notification.
    
    Fixes: E PackageManager: updateAllSharedLibrariesLPw failed: Package
    com.qualcomm.qcrilmsgtunnel requires unavailable shared library
    android.hidl.manager@1.0-java; failing!
    
    Change-Id: Ibc6f943c5e4b85e50ea2b57ae0f85282c14e0fab

--- device.mk
+++ device.mk
@@ -200,6 +200,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
+    android.hidl.manager@1.0-java \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
 
