15546576c35f3fbf9e56a6616ec587819d5bafd6
khanfrd <khanfrd94@gmail.com>
2017-12-12 05:51:51 +0000

    mido: fix init build
    Change-Id: Ib31147d62df84363d2f5f798383ca8263e796e76

--- init/init_msm8953.cpp
+++ init/init_msm8953.cpp
@@ -28,11 +28,15 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/sysinfo.h>
+#include <android-base/logging.h>
 
-#include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 
+namespace android {
+namespace init {
+
+char const *heapstartsize;
 char const *heapgrowthlimit;
 char const *heapminfree;
 
@@ -98,3 +102,5 @@ void vendor_load_properties()
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", "8m");
 }
+}  // namespace init
+}  // namespace android
