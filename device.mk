00acabf67db54e22b7ece38231e142b26186e7e6
Aryan <arykedare@gmail.com>
2019-01-10 23:10:43 +0530

    Drop Telephony

--- device.mk
+++ device.mk
@@ -322,9 +322,6 @@ PRODUCT_PACKAGES += \
     libxml2 \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
