94abd958bed64d1c8a5b27c79fbd1a6917aa8294
Lucchetto <lucchetto.tie@live.com>
2019-02-03 17:22:38 +0100

    Import CAF power hal

--- device.mk
+++ device.mk
@@ -280,7 +280,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service \
+    power.qcom
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
