d4fb36ecf56ecba4b4822180359666b0c31667bc
GiaSen <giammasena@gmail.com>
2019-05-25 13:47:05 +0200

    Revert "Import CAF power hal"
    This reverts commit 94abd958bed64d1c8a5b27c79fbd1a6917aa8294.

--- device.mk
+++ device.mk
@@ -279,9 +279,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.qcom
+    android.hardware.power@1.1-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
