45870c7bd7eddfcc10e48b1e3c8283f6e89c51c1
ZeeLog <Danjel.Zammit@gmail.com>
2019-09-18 02:03:06 +0200

    mido: Drop TARGET_USE_SDCLANG flag
    Change-Id: I9395ff3c8b29a9e07b702af1d7635acfd4c5ab20

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,7 +183,6 @@ TARGET_USES_INTERACTION_BOOST := true
 
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
-TARGET_USE_SDCLANG := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.recovery.qcom
