e909d9acee7751fec37047cecad461349d91e7c6
Akash98Sky <Akash98Sky@users.noreply.github.com>
2019-10-26 10:08:51 +0000

    hidl: build power stats hal
    fix: Cannot find entry android.hardware.power.stats@1.0::IPowerStats/default in either framework or device manifest.

--- device.mk
+++ device.mk
@@ -275,7 +275,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service
+    android.hardware.power@1.2-service \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
